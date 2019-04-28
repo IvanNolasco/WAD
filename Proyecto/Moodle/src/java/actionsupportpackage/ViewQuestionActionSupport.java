a01ecb6a8f4b3ba705ff3a13ef8ac7cc199f36e1
ricardotg34 <loiztg34@gmail.com>
2019-04-27 21:03:55 -0500

    create action and jsp view

---
+++ Proyecto/Moodle/src/java/actionsupportpackage/ViewQuestionActionSupport.java
@@ -0,0 +1,14 @@
+package actionsupportpackage;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ViewQuestionActionSupport extends ActionSupport {
+    
+    public ViewQuestionActionSupport() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}

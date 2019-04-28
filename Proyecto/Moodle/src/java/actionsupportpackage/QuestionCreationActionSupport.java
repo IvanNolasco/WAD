cce39fa6c967ff35a5f260b50fe011a513155df5
IvanNolasco <navi_rc316@hotmail.com>
2019-04-28 17:54:29 -0500

    Sort questions

--- Proyecto/Moodle/src/java/actionsupportpackage/QuestionCreationActionSupport.java
+++ Proyecto/Moodle/src/java/actionsupportpackage/QuestionCreationActionSupport.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -20,19 +21,19 @@ public class QuestionCreationActionSupport extends ActionSupport {
     public QuestionCreationActionSupport() {
     }
     
-    private List<Question> questions; 
+    private List<String> questions; 
     
-    public List<Question> getQuestions() {  
+    public List<String> getQuestions() {  
         return questions;  
     } 
     
-    public void setQuestions(List<Question> questions) {  
+    public void setQuestions(List<String> questions) {  
         this.questions = questions;  
     } 
     
     @Override
     public String execute() throws IOException { 
-        questions = new ArrayList<Question>();
+        questions = new ArrayList<String>();
         
         URL path=this.getClass().getProtectionDomain().getCodeSource().getLocation();
         String pathString = path.toString().replace("build/web/WEB-INF/classes/actionsupportpackage/QuestionCreationActionSupport.class", "jsons/Questions.json/");
@@ -45,12 +46,14 @@ public class QuestionCreationActionSupport extends ActionSupport {
                 JSONObject jsonObject = (JSONObject) q;
                 JSONObject questionJObject = (JSONObject) jsonObject.get("Question");
                 String id = (String) questionJObject.get("id");
-                String name = (String) questionJObject.get("name");
-                String question = (String) questionJObject.get("question");
-                String answer = (String) questionJObject.get("answer");
-                Question questionObject = new Question(id, name, question, answer);
-                questions.add(questionObject);
+                //String name = (String) questionJObject.get("name");
+                //String question = (String) questionJObject.get("question");
+                //String answer = (String) questionJObject.get("answer");
+                //Question questionObject = new Question(id, name, question, answer);
+                questions.add(id);
             }
+            Collections.sort(questions);
+            
             
         }
         catch(Exception e){

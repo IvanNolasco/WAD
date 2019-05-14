9d62fb91f47b9ab376ee79a6fdbe77af2ffac68d
IvanNolasco <navi_rc316@hotmail.com>
2019-05-14 13:35:02 -0500

    read questions from xml

--- Proyecto/Moodle/src/java/actionsupportpackage/QuestionCreationActionSupport.java
+++ Proyecto/Moodle/src/java/actionsupportpackage/QuestionCreationActionSupport.java
@@ -1,6 +1,7 @@
 package actionsupportpackage;
 
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +10,10 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.apache.struts2.ServletActionContext;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 public class QuestionCreationActionSupport extends ActionSupport {
     
@@ -37,7 +42,37 @@ public class QuestionCreationActionSupport extends ActionSupport {
     @Override
     public String execute() throws IOException { 
         questions = new ArrayList<Question>(); 
-        //SE DEFINE LA RUTA DONDE SE VAN A BUSCAR LOS JSON QUE CONTIENEN LA INFORMACION DE LAS PREGUNTAS
+        //se define la ruta donde se va a buscar el archivo XML que contiene las preguntas
+        String path = ServletActionContext.getServletContext().getRealPath("/");
+        try {
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File(path+"\\xmls\\Questions.xml");
+        Document document = builder.build(xmlFile);
+        Element root = document.getRootElement();
+        List teachersList = root.getChildren("teacher");
+            for(int i=0;i<teachersList.size();i++) {
+                Element teacher = (Element)teachersList.get(i);
+                String username = teacher.getAttributeValue("username");  
+                if(username.equals(this.userName)){
+                    List questionsList = teacher.getChildren("question");
+                    for(int j=0;j<questionsList.size();j++){
+                        Element question = (Element)questionsList.get(i);
+                        String id = question.getAttributeValue("id");
+                        String name = question.getAttributeValue("name");
+                        String questionText = question.getAttributeValue("question");
+                        String answer = question.getAttributeValue("answer");
+                        Question questionObject = new Question(id, name, questionText, answer);
+                        questions.add(questionObject);
+                    }
+                    
+                }
+            }
+             
+        }
+        catch(JDOMException e) {
+            e.printStackTrace();
+        }
+        /*
         String pathString = ServletActionContext.getServletContext().getRealPath("/");
         pathString=pathString.replace("build\\web\\", "web\\jsons\\Questions.json\\");
         JSONParser parser = new JSONParser();
@@ -58,7 +93,7 @@ public class QuestionCreationActionSupport extends ActionSupport {
         }
         catch(Exception e){
             e.printStackTrace();
-        }
+        }*/
         return SUCCESS;
     }
 }

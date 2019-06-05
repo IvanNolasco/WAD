a9e94264a4d515841cb4baea240d3e505fc4e15d
IvanNolasco <navi_rc316@hotmail.com>
2019-06-05 17:33:02 -0500

    modifyQuestion2P created

---
+++ Proyecto/Moodle/src/java/actionsupportpackage/ModifyQuestion2PActionSupport.java
@@ -0,0 +1,234 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actionsupportpackage;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.List;
+import org.apache.struts2.ServletActionContext;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+public class ModifyQuestion2PActionSupport extends ActionSupport {
+    
+    private String id;
+    private String qtype;
+    private String name;
+    private String question;
+    private List<Option> optionList;
+    private String maxQuant;
+    private File media;
+    private String mediaContentType;
+    private String mediaFileName;
+    private String initial;
+    private String evaluate;
+    private String correct;
+    private String incorrect;
+    
+    public ModifyQuestion2PActionSupport() {
+    }
+    
+    public String execute() throws Exception {
+        String pathString = ServletActionContext.getServletContext().getRealPath("/");
+        String userName = (String) ServletActionContext.getRequest().getSession().getAttribute("userName");
+        
+        try{           
+            //se define la ruta para escribir el archivo multimedia 
+            File salida = new File(pathString+"media/"+mediaFileName);
+            //se transfiere el archivo multimedia al servidor
+            FileInputStream in = new FileInputStream(media);
+            FileOutputStream out = new FileOutputStream(salida);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+
+            //Write XML
+            SAXBuilder builder = new SAXBuilder();
+            File archivoXML = new File(pathString+"/xmls/Questions.xml");
+            Document documento=builder.build(archivoXML);
+            Element raiz = documento.getRootElement();
+            List lista=raiz.getChildren("teacher");
+            for (Object l : lista) {
+                Element teacher = (Element)l;
+                if (teacher.getAttributeValue("username").equals(userName)) {
+                    Element quest = new Element("question");
+                    quest.setAttribute("id", id);
+                    quest.setAttribute("qtype", qtype);
+                    quest.setAttribute("name", name);
+                    quest.setAttribute("question", question);
+                    quest.setAttribute("max", maxQuant);
+                    quest.setAttribute("source", "media\\"+mediaFileName);
+                    quest.setAttribute("type", mediaContentType);
+                    for(int i=0; i<optionList.size(); i++){
+                        Element option = new Element("option");
+                        option.setAttribute("text", optionList.get(i).getText());
+                        option.setAttribute("points", String.valueOf(optionList.get(i).getPoints()));
+                        quest.addContent(option);
+                    }
+                    teacher.addContent(quest);
+                    break;
+                }
+            }
+            Format formato = Format.getPrettyFormat();
+            XMLOutputter xmloutputter = new XMLOutputter(formato);
+            FileWriter writer= new FileWriter(pathString+"/xmls/Questions.xml");
+            xmloutputter.output(documento, writer);
+            writer.close();
+        }
+        catch(JDOMException e){
+            e.printStackTrace();
+        }
+        
+        try {
+            SAXBuilder builder = new SAXBuilder();
+            File xmlFile = new File(pathString+"\\xmls\\Questions.xml");
+            Document document = builder.build(xmlFile);
+            Element root = document.getRootElement();
+            List teachersList = root.getChildren("teacher");
+            for(int i=0;i<teachersList.size();i++) {
+                Element teacher = (Element)teachersList.get(i);
+                String username = teacher.getAttributeValue("username");  
+                if(username.equals(userName)){
+                    List feedbacksList = teacher.getChildren("feedback");
+                    for(int j=0;j<feedbacksList.size();j++){
+                        Element feedback = (Element)feedbacksList.get(j);
+                        String feedbackid = feedback.getAttributeValue("id");
+                        if(feedbackid.equals(this.id)){
+                            this.correct = feedback.getAttributeValue("correct");
+                            this.incorrect = feedback.getAttributeValue("incorrect");
+                            this.initial = feedback.getAttributeValue("initial");
+                            this.evaluate = feedback.getAttributeValue("evaluate");
+                            break;
+                        }
+                    }
+
+                }
+            }
+             
+        }
+        catch(JDOMException e) {
+            e.printStackTrace();
+        }
+        return SUCCESS;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQtype() {
+        return qtype;
+    }
+
+    public void setQtype(String qtype) {
+        this.qtype = qtype;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<Option> getOptionList() {
+        return optionList;
+    }
+
+    public void setOptionList(List<Option> optionList) {
+        this.optionList = optionList;
+    }
+
+    public String getMaxQuant() {
+        return maxQuant;
+    }
+
+    public void setMaxQuant(String maxQuant) {
+        this.maxQuant = maxQuant;
+    }
+
+    public File getMedia() {
+        return media;
+    }
+
+    public void setMedia(File media) {
+        this.media = media;
+    }
+
+    public String getMediaContentType() {
+        return mediaContentType;
+    }
+
+    public void setMediaContentType(String mediaContentType) {
+        this.mediaContentType = mediaContentType;
+    }
+
+    public String getMediaFileName() {
+        return mediaFileName;
+    }
+
+    public void setMediaFileName(String mediaFileName) {
+        this.mediaFileName = mediaFileName;
+    }
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+
+    public String getEvaluate() {
+        return evaluate;
+    }
+
+    public void setEvaluate(String evaluate) {
+        this.evaluate = evaluate;
+    }
+
+    public String getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(String correct) {
+        this.correct = correct;
+    }
+
+    public String getIncorrect() {
+        return incorrect;
+    }
+
+    public void setIncorrect(String incorrect) {
+        this.incorrect = incorrect;
+    }
+    
+}

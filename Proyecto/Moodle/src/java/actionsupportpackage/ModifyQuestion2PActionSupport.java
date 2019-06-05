1e6c537d1f581d51be6974ba67c154683b9b8c23
IvanNolasco <navi_rc316@hotmail.com>
2019-06-05 17:46:58 -0500

    question eliminated before modify

--- Proyecto/Moodle/src/java/actionsupportpackage/ModifyQuestion2PActionSupport.java
+++ Proyecto/Moodle/src/java/actionsupportpackage/ModifyQuestion2PActionSupport.java
@@ -43,10 +43,13 @@ public class ModifyQuestion2PActionSupport extends ActionSupport {
         String pathString = ServletActionContext.getServletContext().getRealPath("/");
         String userName = (String) ServletActionContext.getRequest().getSession().getAttribute("userName");
         
-        try{           
-            //se define la ruta para escribir el archivo multimedia 
+        int sobre=0; //Variable para sobreescribir el archivo si es necesario
+        
+        //Si el usuario mete un archivo nuevo se crea el file y se guarda en el servidor
+        if(media!=null)
+        {
+            
             File salida = new File(pathString+"media/"+mediaFileName);
-            //se transfiere el archivo multimedia al servidor
             FileInputStream in = new FileInputStream(media);
             FileOutputStream out = new FileOutputStream(salida);
             byte[] buf = new byte[1024];
@@ -56,41 +59,65 @@ public class ModifyQuestion2PActionSupport extends ActionSupport {
             }
             in.close();
             out.close();
-
-            //Write XML
+            sobre=1;
+        }
+        try {
             SAXBuilder builder = new SAXBuilder();
             File archivoXML = new File(pathString+"/xmls/Questions.xml");
             Document documento=builder.build(archivoXML);
-            Element raiz = documento.getRootElement();
-            List lista=raiz.getChildren("teacher");
-            for (Object l : lista) {
-                Element teacher = (Element)l;
-                if (teacher.getAttributeValue("username").equals(userName)) {
-                    Element quest = new Element("question");
-                    quest.setAttribute("id", id);
-                    quest.setAttribute("qtype", qtype);
-                    quest.setAttribute("name", name);
-                    quest.setAttribute("question", question);
-                    quest.setAttribute("max", maxQuant);
-                    quest.setAttribute("source", "media\\"+mediaFileName);
-                    quest.setAttribute("type", mediaContentType);
-                    for(int i=0; i<optionList.size(); i++){
+            //Se crea el nuevo elemento con la informacion modificada
+            Element quest = new Element("question");
+            quest.setAttribute("id", id);
+            quest.setAttribute("qtype", qtype);
+            quest.setAttribute("name", name);
+            quest.setAttribute("question", question);
+            quest.setAttribute("max", maxQuant);
+            for(int i=0; i<optionList.size(); i++){
                         Element option = new Element("option");
                         option.setAttribute("text", optionList.get(i).getText());
                         option.setAttribute("points", String.valueOf(optionList.get(i).getPoints()));
                         quest.addContent(option);
+            }
+            if (sobre == 0)
+                quest.setAttribute("source", mediaFileName);
+            else
+                quest.setAttribute("source", "media\\"+mediaFileName);
+            quest.setAttribute("type", mediaContentType);
+            
+            //Se recupera el elemento raiz
+            Element raiz = documento.getRootElement();
+            //Se recuperan todos los nodos teacher
+            List lista=raiz.getChildren("teacher");
+            //Se recorre cada uno de los teacher
+            for (Object l : lista) {
+                Element teacher = (Element)l;
+                //Se saca el profesor que coincida con el profesor en sesion
+                if (teacher.getAttributeValue("username").equals(userName)) {
+                    //Se listan todas las preguntas creadas por el profesor
+                    List questions = teacher.getChildren("question");
+                    //Se consigue la pregunta que coincida con el id
+                    for (Object qu : questions) {
+                        Element question = (Element)qu;
+                        if (question.getAttributeValue("id").equals(id)) {
+                            if (sobre == 1) {
+                                //al encontrarse la pregunta se elimina el multimedia antiguo asociado a esa pregunta
+                                File file = new File(pathString+question.getAttributeValue("source"));
+                                file.delete();
+                            }
+                            questions.remove(qu);
+                        }
                     }
                     teacher.addContent(quest);
-                    break;
                 }
             }
+            //Se escriben los cambios en el xml
             Format formato = Format.getPrettyFormat();
             XMLOutputter xmloutputter = new XMLOutputter(formato);
             FileWriter writer= new FileWriter(pathString+"/xmls/Questions.xml");
             xmloutputter.output(documento, writer);
             writer.close();
-        }
-        catch(JDOMException e){
+            
+        } catch (JDOMException e) {
             e.printStackTrace();
         }
         
